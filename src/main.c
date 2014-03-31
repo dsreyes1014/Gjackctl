24cb0343ae18cdfc20460591c3c641002d8a4c84
Andrew Reyes <dsreyes1014@gmail.com>
2014-03-31 13:47:16 -0500

    added src directory

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <gtk/gtk.h>
+
+int
+main (int argc, char *argv[])
+{
+	/* Place declarations here */
+	
+	gtk_init (&argc, &argv);
+
+	/* Initialize here */
+
+	gtk_main ();
+
+	return 0;
+}
